96e349e03be3e9e43cb3ffa6f3e3a6aa05f4f69d
codeexpert254 <masaikipkorir@gmail.com>
2021-10-20 08:14:30 +0300

    New JS

---
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -0,0 +1,16 @@
+let country = 'Kenya';
+let population = 40000000;
+let continent = 'Africa';
+
+// console.log(country);
+// console.log(population);
+// console.log(continent);
+
+let isIsland = false;
+
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,5 @@
+let js = 'Esther';
+if(js === 'Esther') alert('I LOVE HER SO MUCH');
+
+40+40+20;
+console.log(40+40+20);
